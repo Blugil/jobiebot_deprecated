44071daf629bb9b8a1ab35c368da297e09d009a1
Blugil <dagnello@asu.edu>
2020-08-31 11:41:07 -0700

    Adjust file format and updated dependencies

--- db/mongo.js
+++ db/mongo.js
@@ -1,8 +1,12 @@
 //creates an instance of the mongo db client with the database link
-const { MongoClient } = require("mongodb");
+const {
+    MongoClient
+} = require("mongodb");
 require('dotenv').config()
-                                                                                
+
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@jobiebot.jihxg.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-const mongo = new MongoClient(url, { useUnifiedTopology: true });
+const mongo = new MongoClient(url, {
+    useUnifiedTopology: true
+});
 
 module.exports = mongo;
