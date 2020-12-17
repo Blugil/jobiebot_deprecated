3477fe6d965f06f2031210ec70e08f46eff89e22
Blugil <dagnello@asu.edu>
2020-12-17 02:51:03 -0700

    update dependencies to latest, fixed bug with playing audio (again)

--- commands/simple/purr.js
+++ commands/simple/purr.js
@@ -16,38 +16,41 @@ class Purr extends commando.Command {
         //pulls the voice object from the member object
         const { voice } = message.member;
 
-        let dispatcher;
         //splits the args string starting after the command prefix
         const args = message.content.slice(this.client.commandPrefix.length).trim().split(' ');
-        //sets variable time to either the value of the second arg or 10000 
-        const time = (args[1] && Number.isInteger(parseInt(args[1])) ? args[1] : 10000);
+        //sets variable time to either the value of the second arg or 10 
+        const time = (args[1] && Number.isInteger(parseInt(args[1])) ? args[1] : 10);
         //checks if user is in voice
         if (voice.channelID) {
             //joins the channel, returns a promise and a connection object
-            await voice.channel.join().then((connection) => {
-                //plays the audio from a youtube video, needs ytdl to play file
-                dispatcher = connection.play(ytdl("https://www.youtube.com/watch?v=CY7t8ow2gOM&t=2", { volume: 0.1 }))
+            voice.channel.join().then((connection) => {
+
+                //url of youtube video to play
+                let url = "https://www.youtube.com/watch?v=CY7t8ow2gOM&t=222s";
+                //plays the audio from a youtube video
+                //the 'highWaterMark: 1 << 25' parameter fixed this, not sure why it didnt work but now it does...don't change it.
+                connection.play(ytdl(url, {quality: 'highestaudio',
+                highWaterMark: 1 << 25}));
+               
+            
             }).catch((error) => {
                 console.log(error);
             });
+
+            //sets a timeout for the bot to disconnect after the specified amount of time or 10 seconds if no time was specified
             setTimeout(() => {
                 try {
-                    dispatcher.destroy();
                     voice.channel.leave();
                 }
                 catch (error) {
                     console.log(error);
                 }
-            }, time);
+            }, time * 1000);
         } else {
             //let user know if they aren't in voice
             await message.channel.send(`Hello, ${message.author}! Jobie can't purr unless his favorite people are with him uwu. Connect to voice so u can listen`);
         }
-
-
     }
 }
-
-
 
 module.exports = Purr;
