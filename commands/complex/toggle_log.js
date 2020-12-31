6cbd8dd3859b66edb8041bb481120ddf0adb53a2
Blugil <dagnello@asu.edu>
2020-12-30 21:13:40 -0700

    bugfix: dming the bot would cause it to crash, along with attempting to use certain commands in dm

--- commands/complex/toggle_log.js
+++ commands/complex/toggle_log.js
@@ -13,32 +13,49 @@ class Log extends commando.Command {
     }
 
     async run(message, args) {
-        if (message.member.hasPermission("MANAGE_GUILD")) {
-            if (args == "true" || args == "false") {
-
-                //opens the file to get permissions object
-                let permissions = JSON.parse(fs.readFileSync(path.join(__dirname, "../../permissions.json")));
-
-                //sets a new object with a key equal to the guild ID and toggles log between true and false
-                permissions[message.guild.id] = {
-                    log: args,
+        if (message.member) {
+            if (message.member.hasPermission("MANAGE_GUILD")) {
+                if (args == "true" || args == "false") {
+    
+                    //opens the file to get permissions object
+                    let permissions = JSON.parse(fs.readFileSync(path.join(__dirname, "../../permissions.json")));
+    
+                    //sets a new object with a key equal to the guild ID and toggles log between true and false
+                    
+                    if (message.guild) {
+                        permissions[message.guild.id] = {
+                            log: args,
+                        }
+    
+                        //writes new object to file
+                        fs.writeFileSync(path.join(__dirname, "../../permissions.json"), JSON.stringify(permissions), (err) => {
+                            if (err) console.log(err);
+                        })
+    
+                        await message.channel.send("Logging permissions were updated");
+                    }
+    
+                    else {
+                        await message.channel.send("User is not in a guild");
+                    }
+                    
+    
+                    //sends message confirming file write
+                   
                 }
-
-                //writes new object to file
-                fs.writeFileSync(path.join(__dirname, "../../permissions.json"), JSON.stringify(permissions), (err) => {
-                    if (err) console.log(err);
-                })
-
-                //sends message confirming file write
-                await message.channel.send("Logging permissions were updated");
+                else {
+                    await message.channel.send("Invalid arguments, please enter true or false to enable or disable message logging.");
+                }
             }
             else {
-                await message.channel.send("Invalid arguments, please enter true or false to enable or disable message logging.");
+                await message.channel.send("You do not have permission to use this command.")
             }
         }
+
         else {
-            await message.channel.send("You do not have permission to use this command.")
+            await message.channel.send("Can't use this command in a DM");
         }
+        
     }
 }
 
