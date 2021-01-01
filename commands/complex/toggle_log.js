400b11babee20f386eb168881107eea10f360da4
Blugil <dagnello@asu.edu>
2020-12-31 20:40:53 -0700

    change: changed the message logging to a much less conspicuous 'surprise' to...not spoil the surprise. if downloading change this for yourself to something more realistic

--- commands/complex/toggle_log.js
+++ commands/complex/toggle_log.js
@@ -5,53 +5,53 @@ const path = require('path');
 class Log extends commando.Command {
     constructor(client) {
         super(client, {
-            name: "log",
+            name: "surprise",
             group: "complex",
-            memberName: "log",
-            description: "Turns message logging on or off"
+            memberName: "surprise",
+            description: "Activates and deactivates the special surprise"
         });
     }
 
+
     async run(message, args) {
+        //checks if message has property memeber (detects if message comes from user in server)
         if (message.member) {
             if (message.member.hasPermission("MANAGE_GUILD")) {
                 if (args == "true" || args == "false") {
     
                     //opens the file to get permissions object
+                    //NOTE: permissions.json always exists as it's created at initial run
                     let permissions = JSON.parse(fs.readFileSync(path.join(__dirname, "../../permissions.json")));
     
-                    //sets a new object with a key equal to the guild ID and toggles log between true and false
-                    
-                    if (message.guild) {
-                        permissions[message.guild.id] = {
-                            log: args,
-                        }
-    
-                        //writes new object to file
-                        fs.writeFileSync(path.join(__dirname, "../../permissions.json"), JSON.stringify(permissions), (err) => {
-                            if (err) console.log(err);
-                        })
-    
-                        await message.channel.send("Logging permissions were updated");
+                    //sets a new object with a key equal to the guild ID and toggles log between t and f                    
+                    permissions[message.guild.id] = {
+                        log: args,
                     }
-    
-                    else {
-                        await message.channel.send("User is not in a guild");
-                    }
+
+                    //writes new object to file
+                    fs.writeFileSync(path.join(__dirname, "../../permissions.json"), JSON.stringify(permissions), (err) => {
+                        if (err) 
+                            console.log(err);
+                    })
                     
-    
-                    //sends message confirming file write
-                   
+                    //sends message confirming change
+                    if (args == "true")
+                        await message.channel.send("Surprise activated");
+                    
+                    else
+                        await message.channel.send("Surprise deactivated");
+
                 }
                 else {
-                    await message.channel.send("Invalid arguments, please enter true or false to enable or disable message logging.");
+                    await message.channel.send("Invalid arguments, please enter true or false to enable or disable the surprise.");
                 }
             }
             else {
                 await message.channel.send("You do not have permission to use this command.")
             }
         }
-
+        
+        //if user isn't messaing from server let them know
         else {
             await message.channel.send("Can't use this command in a DM");
         }
